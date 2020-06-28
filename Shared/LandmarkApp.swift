fae401037e34ced4947bb6aa95c20760f9d05703
clearchaos3 <clearchaos3@gmail.com>
2020-06-27 20:15:42 -0500

    Initial Commit

---
+++ Shared/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Shared
+//
+//  Created by Ryan Lee on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
